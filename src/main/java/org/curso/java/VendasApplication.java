dfa7ac63a8f164c55d6cf26d983c60c2699d01ac
Alan Oliveira <alanjhone@gmail.com>
2020-08-15 12:37:38 -0300

    Trabalhando com jdbcTemplate

--- src/main/java/org/curso/java/VendasApplication.java
+++ src/main/java/org/curso/java/VendasApplication.java
@@ -1,10 +1,51 @@
 package org.curso.java;
 
+import org.curso.java.domain.entity.Cliente;
+import org.curso.java.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Alan Oliveira"));
+            clientes.salvar(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+//            System.out.println("Deletando clientes");
+//            clientes.obterTodos().forEach(clientes::deletar);
+
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()) {
+                System.out.println("Nenhum cliente encontrado.");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
