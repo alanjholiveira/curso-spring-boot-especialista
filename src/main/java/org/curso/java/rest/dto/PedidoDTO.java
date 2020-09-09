ed90db3e6cd64b469d4572e6c1373e054d1b4fc2
Alan Oliveira <alan.oliveira@basis.com.br>
2020-09-09 14:33:39 -0300

    usando lombok nos DTOs

--- src/main/java/org/curso/java/rest/dto/PedidoDTO.java
+++ src/main/java/org/curso/java/rest/dto/PedidoDTO.java
@@ -1,9 +1,16 @@
 package org.curso.java.rest.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PedidoDTO {
 
     private Integer cliente;
