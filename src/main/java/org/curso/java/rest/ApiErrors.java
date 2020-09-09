47110d1badb77d488875332513e4934c7b084cb5
Alan Oliveira <alan.oliveira@basis.com.br>
2020-09-09 14:29:47 -0300

    Implementado ExcpetionHandlers

---
+++ src/main/java/org/curso/java/rest/ApiErrors.java
@@ -0,0 +1,17 @@
+package org.curso.java.rest;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String messagemError) {
+        this.errors = Arrays.asList(messagemError);
+    }
+}
