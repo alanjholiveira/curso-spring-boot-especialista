aa48918fa258593c33ec84f3e26c144cdda1667b
Alan Oliveira <alan.oliveira@basis.com.br>
2020-09-10 11:18:34 -0300

    usando Bea Validation no cadastro do cliente

--- src/main/java/org/curso/java/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/org/curso/java/rest/controller/ApplicationControllerAdvice.java
@@ -4,24 +4,40 @@ import org.curso.java.exception.PedidoNaoEncontradoException;
 import org.curso.java.exception.RegraNegocioException;
 import org.curso.java.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
     @ExceptionHandler(RegraNegocioException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
         String messagemErro = ex.getMessage();
         return new ApiErrors(messagemErro);
     }
 
     @ExceptionHandler(PedidoNaoEncontradoException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     public ApiErrors handlePedidoNotFoundException(PedidoNaoEncontradoException ex) {
         String messagemErro = ex.getMessage();
         return new ApiErrors(messagemErro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
+        List<String> erros = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage()).collect(Collectors.toList());
+        return new ApiErrors(erros);
     }
 }
